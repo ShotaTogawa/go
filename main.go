066910c62401c5de0da22e5a20ae38932aa92c90
ShotaTogawa <s.togawa0112@gmail.com>
2021-01-16 18:52:16 +0900

    add: user関連

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"./config"
+	"./app/models"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
+
+	// u := models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "texttext"
+
+	u, _ := models.GetUser(1)
+	u.Name = "updateUser"
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+
+	fmt.Println(u)
 }
